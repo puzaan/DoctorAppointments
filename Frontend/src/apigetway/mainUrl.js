e78051ecc8bddfddda17eb0cecfa9627f7a92982
puzaan <hakku.stha22@gmail.com>
2022-08-11 14:16:36 +0545

    api url changed

--- Frontend/src/apigetway/mainUrl.js
+++ Frontend/src/apigetway/mainUrl.js
@@ -1,3 +1,6 @@
-const url = 'https://doctorappointment123.herokuapp.com';
+// const url = 'https://doctorappointment123.herokuapp.com';
+
+const url = "http://157.245.110.191";
+
 
 export default url;
