9a7b839f6653af9745b0b9e84f323d344b6cb014
puzaan <hakku.stha22@gmail.com>
2022-08-21 13:18:41 +0545

    updated content

--- Frontend/src/pages/DoctorLogin.js
+++ Frontend/src/pages/DoctorLogin.js
@@ -1,61 +1,61 @@
 // @mui
-import { styled } from '@mui/material/styles';
-import { Card, Container, Typography } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Card, Container, Typography } from "@mui/material";
 // hooks
-import useResponsive from '../hooks/useResponsive';
+import useResponsive from "../hooks/useResponsive";
 // components
-import Page from '../components/Page';
-import Logo from '../components/Logo';
+import Page from "../components/Page";
+import Logo from "../components/Logo";
 // sections
-import { DoctorLoginForm } from '../sections/auth/DoctorLoginForm';
+import { DoctorLoginForm } from "../sections/auth/DoctorLoginForm";
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex',
+const RootStyle = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
   },
 }));
 
-const HeaderStyle = styled('header')(({ theme }) => ({
+const HeaderStyle = styled("header")(({ theme }) => ({
   top: 0,
   zIndex: 9,
   lineHeight: 0,
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  position: 'absolute',
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  position: "absolute",
   padding: theme.spacing(3),
-  justifyContent: 'space-between',
-  [theme.breakpoints.up('md')]: {
-    alignItems: 'flex-start',
+  justifyContent: "space-between",
+  [theme.breakpoints.up("md")]: {
+    alignItems: "flex-start",
     padding: theme.spacing(7, 5, 0, 7),
   },
 }));
 
 const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
+  width: "100%",
   maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   margin: theme.spacing(2, 0, 2, 2),
 }));
 
-const ContentStyle = styled('div')(({ theme }) => ({
+const ContentStyle = styled("div")(({ theme }) => ({
   maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
+  margin: "auto",
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
   padding: theme.spacing(12, 0),
 }));
 
 // ----------------------------------------------------------------------
 
 export default function DoctorLogin() {
-  const mdUp = useResponsive('up', 'md');
+  const mdUp = useResponsive("up", "md");
 
   return (
     <Page title="DoctorLogin">
@@ -68,9 +68,12 @@ export default function DoctorLogin() {
         {mdUp && (
           <SectionStyle>
             <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Hi, Welcome Back XYB Doctor Page
+              Hi, Welcome Back To XYBA Doctor Page.
             </Typography>
-            <img src="/static/illustrations/illustration_register.png" alt="login" />
+            <img
+              src="/static/illustrations/illustration_register.png"
+              alt="login"
+            />
           </SectionStyle>
         )}
         <Container maxWidth="sm">
@@ -78,7 +81,9 @@ export default function DoctorLogin() {
             <Typography variant="h4" gutterBottom>
               Sign in to Doctor Page
             </Typography>
-            <Typography sx={{ color: 'text.secondary', mb: 5 }}>Enter your details below.</Typography>
+            <Typography sx={{ color: "text.secondary", mb: 5 }}>
+              Enter your details below.
+            </Typography>
             <DoctorLoginForm paths="/doctor/booking" />
           </ContentStyle>
         </Container>
--- Frontend/src/pages/SuperAdminLogin.js
+++ Frontend/src/pages/SuperAdminLogin.js
@@ -1,60 +1,60 @@
 // @mui
-import { styled, Card, Container, Typography } from '@mui/material';
+import { styled, Card, Container, Typography } from "@mui/material";
 // hooks
-import useResponsive from '../hooks/useResponsive';
+import useResponsive from "../hooks/useResponsive";
 // components
-import Page from '../components/Page';
-import Logo from '../components/Logo';
+import Page from "../components/Page";
+import Logo from "../components/Logo";
 // sections
-import { SuperLoginForm } from '../sections/auth/superAdminLogin';
+import { SuperLoginForm } from "../sections/auth/superAdminLogin";
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex',
+const RootStyle = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
   },
 }));
 
-const HeaderStyle = styled('header')(({ theme }) => ({
+const HeaderStyle = styled("header")(({ theme }) => ({
   top: 0,
   zIndex: 9,
   lineHeight: 0,
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  position: 'absolute',
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  position: "absolute",
   padding: theme.spacing(3),
-  justifyContent: 'space-between',
-  [theme.breakpoints.up('md')]: {
-    alignItems: 'flex-start',
+  justifyContent: "space-between",
+  [theme.breakpoints.up("md")]: {
+    alignItems: "flex-start",
     padding: theme.spacing(7, 5, 0, 7),
   },
 }));
 
 const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
+  width: "100%",
   maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   margin: theme.spacing(2, 0, 2, 2),
 }));
 
-const ContentStyle = styled('div')(({ theme }) => ({
+const ContentStyle = styled("div")(({ theme }) => ({
   maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
+  margin: "auto",
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
   padding: theme.spacing(12, 0),
 }));
 
 // ----------------------------------------------------------------------
 
 export default function SuperAdminLogin() {
-  const mdUp = useResponsive('up', 'md');
+  const mdUp = useResponsive("up", "md");
 
   return (
     <Page title="Login">
@@ -70,9 +70,12 @@ export default function SuperAdminLogin() {
         {mdUp && (
           <SectionStyle>
             <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Hi, Welcome Back XYB Super Admin Page
+              Hi, Welcome Back TO XYBA Super Admin Page
             </Typography>
-            <img src="/static/illustrations/illustration_login.png" alt="login" />
+            <img
+              src="/static/illustrations/illustration_login.png"
+              alt="login"
+            />
           </SectionStyle>
         )}
 
@@ -81,7 +84,9 @@ export default function SuperAdminLogin() {
             <Typography variant="h4" gutterBottom>
               Sign in to Super Admin Page
             </Typography>
-            <Typography sx={{ color: 'text.secondary', mb: 5 }}>Enter your details below.</Typography>
+            <Typography sx={{ color: "text.secondary", mb: 5 }}>
+              Enter your details below.
+            </Typography>
             <SuperLoginForm paths="/superadmin/app" />
           </ContentStyle>
         </Container>
